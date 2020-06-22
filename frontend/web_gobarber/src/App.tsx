52a49aad0d83cc24c1ab8b843f07fbedc5506fa2
Welton Castoldi <weltoncastoldi@hotmail.com>
2020-06-22 18:20:21 -0300

    error: eslint styled-components

--- frontend/web_gobarber/src/App.tsx
+++ frontend/web_gobarber/src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>OPa</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>OPa</h1>
+    <GlobalStyle></GlobalStyle>
+  </>
+);
 
 export default App;
---
+++ frontend/web_gobarber/src/pages/Signin/index.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => <Container></Container>;
+
+export default SignIn;
