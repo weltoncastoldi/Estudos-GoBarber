ed982608c5766e8035423e6a130c40b3c0e10849
Welton Castoldi <weltoncastoldi@hotmail.com>
2020-08-03 10:58:46 -0300

    feat: Create Appointments Test an Config Jest.

---
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    appointment.id = uuid();
+    appointment.date = date;
+    appointment.provider_id = provider_id;
+
+    this.appointments.push(appointment);
+    return appointment;
+  }
+
+  public async findBydate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+}
+
+export default AppointmentsRepository;
